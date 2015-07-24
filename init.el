dfb08daf2add46f7e627198d30c8dcea08b00e5f
Sebastien Lahaie <sebastien.lahaie@gmail.com>
2015-07-24 19:12:24 -0400

    added auctex configuration

--- init.el
+++ init.el
@@ -16,7 +16,7 @@
 
 ;; make more packages available with the package installer
 (setq to-install
-      '(python-mode autopair auto-complete flycheck jedi fill-column-indicator smooth-scrolling flx-ido projectile workgroups ipython ein heroku-theme))
+      '(python-mode autopair auto-complete flycheck jedi fill-column-indicator smooth-scrolling flx-ido projectile workgroups ipython ein heroku-theme auctex))
 (mapc 'install-if-needed to-install)
 
 (require 'autopair)
@@ -69,6 +69,132 @@
 ;;;; Ipython notebook
 (require 'ein)
 (setq ein:use-auto-complete t)
+
+;;;; AUCTeX
+
+(setq TeX-auto-save t)
+(setq TeX-parse-self t)
+;;(setq-default TeX-master nil)
+(add-hook 'LaTeX-mode-hook 'visual-line-mode)
+(add-hook 'LaTeX-mode-hook 'flyspell-mode)
+(add-hook 'LaTeX-mode-hook 'LaTeX-math-mode)
+(add-hook 'LaTeX-mode-hook 'turn-on-reftex)
+(setq reftex-plug-into-AUCTeX t)
+;; use pdflatex
+(setq TeX-PDF-mode t) 
+ 
+(add-hook 'LaTeX-mode-hook 'turn-on-auto-fill) 
+(add-hook 'LaTeX-mode-hook (lambda () (abbrev-mode +1))) 
+(server-start)
+
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+;; FROM: http://www.emacswiki.org/emacs/TN
+(require 'tex-buf)
+(defun TeX-command-default (name)
+  "Next TeX command to use. Most of the code is stolen from `TeX-command-query'."
+  (cond ((if (string-equal name TeX-region)
+             (TeX-check-files (concat name "." (TeX-output-extension))
+                              (list name)
+                              TeX-file-extensions)
+           (TeX-save-document (TeX-master-file)))
+         TeX-command-default)
+        ((and (memq major-mode '(doctex-mode latex-mode))
+              (TeX-check-files (concat name ".bbl")
+                               (mapcar 'car
+                                       (LaTeX-bibliography-list))
+                               BibTeX-file-extensions))
+         ;; We should check for bst files here as well.
+         TeX-command-BibTeX)
+        ((TeX-process-get-variable name
+                                   'TeX-command-next
+                                   TeX-command-Show))
+        (TeX-command-Show)))
+
+(defcustom TeX-texify-Show t
+  "Start view-command at end of TeX-texify?"
+  :type 'boolean
+  :group 'TeX-command)
+
+(defcustom TeX-texify-max-runs-same-command 5
+  "Maximal run number of the same command"
+  :type 'integer
+  :group 'TeX-command)
+
+(defun TeX-texify-sentinel (&optional proc sentinel)
+  "Non-interactive! Call the standard-sentinel of the current LaTeX-process.
+If there is still something left do do start the next latex-command."
+  (set-buffer (process-buffer proc))
+  (funcall TeX-texify-sentinel proc sentinel)
+  (let ((case-fold-search nil))
+    (when (string-match "\\(finished\\|exited\\)" sentinel)
+      (set-buffer TeX-command-buffer)
+      (unless (plist-get TeX-error-report-switches (intern (TeX-master-file)))
+        (TeX-texify)))))
+
+(defun TeX-texify ()
+  "Get everything done."
+  (interactive)
+  (let ((nextCmd (TeX-command-default (TeX-master-file)))
+        proc)
+    (if (and (null TeX-texify-Show)
+             (equal nextCmd TeX-command-Show))
+        (when  (called-interactively-p 'any)
+          (message "TeX-texify: Nothing to be done."))
+      (TeX-command nextCmd 'TeX-master-file)
+      (when (or (called-interactively-p 'any)
+                (null (boundp 'TeX-texify-count-same-command))
+                (null (boundp 'TeX-texify-last-command))
+                (null (equal nextCmd TeX-texify-last-command)))
+        (mapc 'make-local-variable '(TeX-texify-sentinel TeX-texify-count-same-command TeX-texify-last-command))
+        (setq TeX-texify-count-same-command 1))
+      (if (>= TeX-texify-count-same-command TeX-texify-max-runs-same-command)
+          (message "TeX-texify: Did %S already %d times. Don't want to do it anymore." TeX-texify-last-command TeX-texify-count-same-command)
+        (setq TeX-texify-count-same-command (1+ TeX-texify-count-same-command))
+        (setq TeX-texify-last-command nextCmd)
+        (and (null (equal nextCmd TeX-command-Show))
+             (setq proc (get-buffer-process (current-buffer)))
+             (setq TeX-texify-sentinel (process-sentinel proc))
+             (set-process-sentinel proc 'TeX-texify-sentinel))))))
+
+; don't run View after compiling
+(setq TeX-texify-Show nil)
+
+; key hook for compiling (C-c C-a)
+(add-hook 'LaTeX-mode-hook 
+    '(lambda () (local-set-key (kbd "C-c C-a") 'TeX-texify)))
+
+(defun TeX-save-and-texify ()
+  "Save and then run texify."
+  (interactive)
+  (save-buffer)
+  (TeX-texify))
+
+; NOTE: place this AFTER all AUCTeX key bindings are complete
+;
+; key hook for save and compile (replaces C-x C-s)
+; this hooks when opened file has extension .tex
+; (b/c hooking on 'LaTeX-mode-hook will cause .cls files to be compiled too)
+(add-hook 'find-file-hook 
+    ; only execute when file extension matches .tex
+    '(lambda() (when (and (stringp buffer-file-name)
+                          (string-match "\\.tex\\'" buffer-file-name))
+         ; make buffer-local copy of local-key-map
+         ; this is b/c local key maps are for entire major modes
+         (use-local-map (copy-keymap LaTeX-mode-map))
+         (local-set-key (kbd "C-x C-s") 'TeX-save-and-texify))))
+
+
+
+
+
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 
 ;;;; Jedi settings
 (require 'jedi)
@@ -172,6 +298,22 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(TeX-source-correlate-method (quote synctex))
+ '(TeX-source-correlate-mode t)
+ '(TeX-view-program-list
+   (quote
+    (("Sumatra PDF"
+      ("\"C:/Program Files (x86)/SumatraPDF/SumatraPDF.exe\" -reuse-instance"
+       (mode-io-correlate " -forward-search %b %n")
+       " %o")
+      nil))))
+ '(TeX-view-program-selection
+   (quote
+    (((output-dvi style-pstricks)
+      "dvips and gv")
+     (output-dvi "xdvi")
+     (output-pdf "Sumatra PDF")
+     (output-html "xdg-open"))))
  '(ansi-color-names-vector
    ["#3F3F3F" "#CC9393" "#7F9F7F" "#F0DFAF" "#8CD0D3" "#DC8CC3" "#93E0E3" "#DCDCCC"])
  '(blink-cursor-mode nil)
