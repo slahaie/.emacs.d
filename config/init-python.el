8f9a6222825d9a69605f7e329926858441a785ae
Sebastien Lahaie <sebastien.lahaie@gmail.com>
2015-08-04 00:25:08 -0400

    switched back to regular python shell

--- config/init-python.el
+++ config/init-python.el
@@ -4,9 +4,9 @@
 (elpy-enable)
 (elpy-use-ipython)
 (setq elpy-rpc-backend "jedi")
-(setq python-shell-interpreter "C:\\python27\\python.exe"
-      python-shell-interpreter-args
-      "-i C:\\python27\\Scripts\\ipython-script.py console --pylab=qt")
+;; (setq python-shell-interpreter "C:\\python27\\python.exe"
+;;       python-shell-interpreter-args
+;;       "-i C:\\python27\\Scripts\\ipython-script.py console --pylab=qt")
 (eval-after-load 'elpy
   '(progn
      (define-key elpy-mode-map (kbd "M-,") 'pop-tag-mark)))
