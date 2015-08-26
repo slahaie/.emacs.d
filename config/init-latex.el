cd318c16937b40c9d69ef374bc9aef0b11dd379d
Sebastien Lahaie <sebastien.lahaie@gmail.com>
2015-08-26 14:59:25 -0400

    removed hook to flyspell in latex mode

--- config/init-latex.el
+++ config/init-latex.el
@@ -6,7 +6,7 @@
 (setq TeX-parse-self t)
 ;;(setq-default TeX-master t)
 (add-hook 'LaTeX-mode-hook 'visual-line-mode)
-(add-hook 'LaTeX-mode-hook 'flyspell-mode)
+;;(add-hook 'LaTeX-mode-hook 'flyspell-mode)
 (add-hook 'LaTeX-mode-hook 'LaTeX-math-mode)
 (add-hook 'LaTeX-mode-hook 'turn-on-reftex)
 (setq reftex-plug-into-AUCTeX t)
