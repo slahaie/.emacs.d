4b77f4ec40f68768a254d047f42b569e087675bb
slahaie <sebastien.lahaie@gmail.com>
2015-10-06 11:35:55 -0400

    fixed dos end of lines showing

--- config/init-misc.el
+++ config/init-misc.el
@@ -90,3 +90,13 @@
 ;; short yes or no query
 (defalias 'yes-or-no-p 'y-or-n-p)
 
+;; remove DOS line endings
+(defun remove-dos-eol ()
+  "Do not show ^M in files containing mixed UNIX and DOS line endings."
+  (interactive)
+  (setq buffer-display-table (make-display-table))
+  (aset buffer-display-table ?\^M []))
+(add-hook 'text-mode-hook 'remove-dos-eol)
+(add-hook 'emacs-lisp-mode-hook 'remove-dos-eol)
+
+
